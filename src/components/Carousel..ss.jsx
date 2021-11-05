bc325d38401741053f1018d2982e9c2c675b1453
Joan Maina <mainajoan12@gmail.com>
2021-11-05 13:39:45 +0300

    improve home page UI

---
+++ src/components/Carousel..ss.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Carousel = () => {
+  return (
+    <div
+      id="carouselExampleCaptions"
+      class="carousel slide"
+      data-bs-ride="carousel"
+      style={{ height: "60vh", overflowY: "hidden" }}
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="https://images.unsplash.com/photo-1539057307452-65f8bc136475?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+            class="d-block w-100"
+            alt="..."
+            style={{ height: "100%", width: "100%" }}
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://images.unsplash.com/photo-1539057307452-65f8bc136475?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+            class="d-block w-100"
+            alt="..."
+            style={{ height: "100%", width: "100%" }}
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="https://images.unsplash.com/photo-1539057307452-65f8bc136475?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80"
+            class="d-block w-100"
+            alt="..."
+            style={{ height: "100%", width: "100%" }}
+          />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,47 +1,70 @@
-
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux';
-import { useState } from 'react';
-import '../styles/Display.css'
-import passat from '../images/passat1.JPG'
-import { addToCart } from '../redux/actions/DisplayAction'
+import React from "react";
+import { addToCart, getCarDescription } from "../redux/actions/DisplayAction";
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import "../styles/Display.css";
+import { Link } from "react-router-dom";
 
 function Display() {
-
   const dispatch = useDispatch();
 
-    const state = useSelector(state => state);
+  const state = useSelector((state) => state);
 
-     const [showCarDetails, setShowCarDetails] = useState(false);
+  const [showCarDetails, setShowCarDetails] = useState(false);
 
-     const [searchItem, setSearchItem] = useState('');
+  const [searchItem, setSearchItem] = useState("");
 
-    return (
-        <div >
-          <div className="search">
-            <input type='text' placeholder="Search.." onChange={(event)=>{
-              setSearchItem(event.target.value)
-            }}></input>
-            <button><i class="fa fa-search-plus" aria-hidden="true"></i></button>
-            </div>
-            <div className="main-container">
-            {/* <h1>joan</h1> */}
-            {state.cars.filter((car) => {
-              if(searchItem == ''){
-                return car
-              }else if(car.carMake.toLowerCase().includes(searchItem.toLowerCase())){
-                return car;
-              }}).map(car => (
-                <div key = {car.carId} className="singleCar">
-                   <p className="carMake">Make: {car.carMake}</p>
-                    <img src={passat} className="carImage" alt={car.carMake} onClick = {() => setShowCarDetails(!showCarDetails)}/>
-                    <p>Mileage:{car.carMileage}</p>
-                   
-                    <p>Year of Production: {car.carYear}</p>
-                    <p>Price Offer: {car.carPrice}</p>
-                    <button className="addToCartButton"  onClick={() => dispatch(addToCart(car.carId))}>ADD TO CART</button>
-                   
-                    {/* <CarDetails trigger = {showCarDetails} setTrigger = {setShowCarDetails}>
+  console.log(state.cars);
+  return (
+    <div className="display">
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search.."
+          onChange={(event) => {
+            setSearchItem(event.target.value);
+          }}
+        ></input>
+
+        <button>
+          <i class="fa fa-search-plus" aria-hidden="true"></i>
+        </button>
+      </div>
+      <div className="main-container">
+        {/* <h1>joan</h1> */}
+        {state.cars
+          .filter((car) => {
+            if (searchItem == "") {
+              return car;
+            } else if (
+              car.carMake.toLowerCase().includes(searchItem.toLowerCase())
+            ) {
+              return car;
+            }
+          })
+          .map((car) => (
+            <div key={car.carId} className="singleCar">
+              <p className="carMake">Make: {car.carMake}</p>
+              <Link to="description">
+                <img
+                  src={car.carImage}
+                  className="carImage"
+                  alt={car.carMake}
+                  onClick={() => dispatch(getCarDescription(car.carId))}
+                />
+              </Link>
+              <p>Mileage:{car.carMileage}</p>
+              {/* <p>{car.carImage}</p> */}
+              <p>Year of Production: {car.carYear}</p>
+              <p>Price Offer: {car.carPrice}</p>
+              <button
+                className="addToCartButton"
+                onClick={() => dispatch(addToCart(car.carId))}
+              >
+                ADD TO CART
+              </button>
+
+              {/* <CarDetails trigger = {showCarDetails} setTrigger = {setShowCarDetails}>
 
                       <Modal.Dialog>
                         <Modal.Header>
@@ -58,12 +81,11 @@ function Display() {
                         </Modal.Footer>
                       </Modal.Dialog>
                     </CarDetails> */}
-                </div>
-              
-            ) )}
             </div>
-        </div>
-    )
+          ))}
+      </div>
+    </div>
+  );
 }
 
-export default Display
+export default Display;
